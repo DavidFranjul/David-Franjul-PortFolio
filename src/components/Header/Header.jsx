1a9238db19dd6c0eeedf590364840d5de9eef5c9
DavidFranjul <davidfranjul09@gmail.com>
2023-02-17 15:10:33 -0400

    test commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png";
+// import ME from "../../assets/me.png";
 import meSMALL from "../../assets/me-small.png";
 import Headersocials from "./Headersocials";
 
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="header-top">
           <h5>Hello I'm</h5>
           <h1>David Franjul</h1>
-          <h5 className="text-light">TEST TEST TEST</h5>
+          <h5 className="text-light">TEST TEST</h5>
           <CTA />
           <Headersocials />
         </div>
