b1fdf43949d6496834afe165ff01dd299119c36e
DavidFranjul <davidfranjul09@gmail.com>
2023-02-17 11:53:02 -0400

    Test change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
-import meSMALL from "../../assets/me-small.png"
+import meSMALL from "../../assets/me-small.png";
 import Headersocials from "./Headersocials";
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="header-top">
           <h5>Hello I'm</h5>
           <h1>David Franjul</h1>
-          <h5 className="text-light">Front End Developer</h5>
+          <h5 className="text-light">TEST TEST TEST</h5>
           <CTA />
           <Headersocials />
         </div>
