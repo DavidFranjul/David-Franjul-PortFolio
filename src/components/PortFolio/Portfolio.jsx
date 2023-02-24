8f49850dfac64eff269ad6ffcec67b66d49f62dc
DavidFranjul <davidfranjul09@gmail.com>
2023-02-24 11:57:12 -0400

    added background color to all btns

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -1,87 +1,95 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg"
-import IMG2 from "../../assets/portfolio2.jpg"
-import IMG3 from "../../assets/portfolio3.jpg"
-import IMG4 from "../../assets/portfolio4.jpg"
-import IMG5 from "../../assets/portfolio5.png"
-import IMG6 from "../../assets/portfolio6.jpg"
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto Currency dashboard',
+    title: "Crypto Currency dashboard",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Charts templates',
+    title: "Charts templates",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Figma dashboard',
+    title: "Figma dashboard",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Mantaining tasks',
+    title: "Mantaining tasks",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Charts templates and infographics',
+    title: "Charts templates and infographics",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
   {
     id: 6,
     image: IMG6,
-    title: 'Crypto Currency dashboard',
+    title: "Crypto Currency dashboard",
     github: "https://github.com",
-    demo: "dribbble.com"
+    demo: "dribbble.com",
   },
-
-]
+];
 
 const Portfolio = () => {
-  return <section id="portfolio">
-    <h5>My recent Work</h5>
-    <h2>Portfolio</h2>
+  return (
+    <section id="portfolio">
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
 
-    <div className="container portfolio__container">
-
-      {
-        data.map(({ id, image, title, github, demo }) => {
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
-                <img src={image} alt={title} className="portfolio__item-image1" />
+                <img
+                  src={image}
+                  alt={title}
+                  className="portfolio__item-image1"
+                />
               </div>
 
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-                <a href={github} className="btn btn-primary ">Github</a>
-                <a href={demo} rel="noreferrer" className="btn " target="_blank">Live Demo</a>
+                <a href={github} className="btn btn-primary btn-background">
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  rel="noreferrer"
+                  className="btn "
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
               </div>
-            </article>)
-        })
-      }
-
-
-    </div>
-
-
-  </section>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
